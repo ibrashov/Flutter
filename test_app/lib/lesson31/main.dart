9d69070d7ea324c7d8b6261d6bea6a0cbcfe7417
ibrashov <ibrashovanuar@gmail.com>
2026-07-02 15:45:55 +0500

    Lesson 31's part

---
+++ test_app/lib/lesson31/main.dart
@@ -0,0 +1,54 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() {
+  runApp(const LocalJsonPracticeApp());
+}
+
+class Task {
+  final int id;
+  final String title;
+  final String description;
+  final bool isDone;
+
+  const Task({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.isDone,
+  });
+
+  factory Task.fromJson(Map<String, dynamic> json) {
+    return Task(
+      id: json['id'] ?? 0,
+      title: json['title'] ?? '',
+      description: json['description'] ?? '',
+      isDone: json['isDone'] ?? false,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'description': description,
+      'isDone': isDone,
+    };
+  }
+
+  Task copyWith({
+    int? id,
+    String? title,
+    String? description,
+    bool? isDone,
+  }) {
+    return Task(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      description: description ?? this.description,
+      isDone: isDone ?? this.isDone,
+    );
+  }
+}
